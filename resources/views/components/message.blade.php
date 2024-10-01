917245c22109d6dcdb81f3d141d5cfa9b767fff6
alias <matusoyo2436@gmail.com>
2024-10-01 19:25:30 +0900

    added new file&text
    section10 10-3
    
    @if ($message)
        <div class="p-4 m-2 rounded bg-green-100">
        {{$message}}
        </div>
    @endif
    <style>
        .bg-green-100 {
            background-color: #d1fae5; /* 緑の背景色を指定 */
        }
    </style>

---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,10 @@
+@if ($message)
+    <div class="p-4 m-2 rounded bg-green-100">
+    {{$message}}
+    </div>
+@endif
+<style>
+    .bg-green-100 {
+        background-color: #d1fae5; /* 緑の背景色を指定 */
+    }
+</style>
