0b620c6353726980cac69e0aa91a812b25a50109
alias <matusoyo2436@gmail.com>
2024-10-01 13:44:01 +0900

    added new text
    section7 7-3
    
        public function index() {
            $posts = Post::all();
            $posts = Post::where('user_id', auth()->id())->get();
            return view('post.index', compact('posts'));
        }
    }
    
    // postsテーブルのログインユーザーのデータを取得
            // $posts=Post::where('user_id', auth()->id())->get();
    
            // postsテーブルのログインユーザー以外のデータを取得
            // $posts=Post::where('user_id', '!=', auth()->id())->get();
    
            // postsテーブルの2022/12/2以降のデータを取得
            // $posts=Post::whereDate('created_at', '>=', '2022-12-02')->get();
    
            // postsテーブルのuser_idが1で2022/12/2以降のデータを取得
            // $posts=Post::where('user_id', 1)->whereDate('created_at', '>=', '2022-12-02')->get();
    
            // postsテーブルのすべてのデータを新しい順に取得
            // $posts=Post::orderBy('created_at', 'desc')->get();

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,6 +26,7 @@ class PostController extends Controller
 
     public function index() {
         $posts = Post::all();
+        $posts = Post::where('user_id', auth()->id())->get();
         return view('post.index', compact('posts'));
     }
 }
